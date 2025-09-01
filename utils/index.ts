4cd45d58ac6bda22c609a1fa8d6bfcae82e2ae27
UtekosKristoffer <205621009+UtekosKristoffer@users.noreply.github.com>
2025-09-01 22:07:20 +0200

    Create utils module with utility functions
    Added utility functions for various tasks.

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+//Path: src/lib/utils/index.ts
+
+/**
+ * @file src/lib/utils/index.ts
+ * @module utils
+ * @description Utility functions for various tasks
+ * @function menuReducer
+ * @function className
+ * @function safeJsonParse
+ */
+
+export { default as className } from '@/lib/utils/className'
+export { className as cn } from '@/lib/utils/className'
+export { menuReducer } from '@/lib/utils/menuReducer'
+export { safeJsonParse } from '@/lib/utils/safeJsonParse'
+export { formatPrice } from '@/lib/utils/formatPrice'
